057dca9ee791df9b3405358adcdc3e86b45cdf87
Aman Kumar <amanrajkumar4@gmail.com>
2024-09-14 22:08:31 +0530

    Parsing Command Line Arguments

--- test-projects/node_modules/tools/bin/index.js
+++ test-projects/node_modules/tools/bin/index.js
@@ -1,3 +1,29 @@
 #! /usr/bin/env node
 
-console.log("Hello CLI User!!")
+// console.log("Hello CLI User!!")
+// console.log(process.argv);
+
+const arg = require('arg');
+
+try {
+    const args = arg({
+        '--start': Boolean,
+        '--build': Boolean,
+    });
+
+    // console.log(args);
+
+    if (args['--start']) {
+        console.log("starting the application");
+    }
+} catch (e) {
+    console.log(e.message);
+    console.log();
+    usage();
+}
+
+function usage(){
+    console.log(`tool [cmd]
+        --start\tStarts the application
+        --build\tBuilds the application`);
+}
