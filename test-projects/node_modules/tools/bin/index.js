299e0e3ddd04b5be711d2c2dabe5f8b7dd99b3ab
Aman Kumar <amanrajkumar4@gmail.com>
2024-09-15 22:14:52 +0530

    Adding color to terminal output

--- test-projects/node_modules/tools/bin/index.js
+++ test-projects/node_modules/tools/bin/index.js
@@ -4,6 +4,7 @@
 // console.log(process.argv);
 
 const arg = require('arg');
+const chalk = require('chalk');
 
 try {
     const args = arg({
@@ -14,16 +15,21 @@ try {
     // console.log(args);
 
     if (args['--start']) {
-        console.log("starting the application");
+        // console.log("starting the application");
+        console.log(chalk.bgCyanBright('Starting the application'));
     }
 } catch (e) {
-    console.log(e.message);
+    // console.log(e.message);
+    console.log(chalk.yellow(e.message));
     console.log();
     usage();
 }
 
 function usage(){
-    console.log(`tool [cmd]
-        --start\tStarts the application
-        --build\tBuilds the application`);
+    // console.log(`tool [cmd]
+    //     --start\tStarts the application
+    //     --build\tBuilds the application`);
+    console.log(`${chalk.whiteBright('tool [CMD]')}
+    ${chalk.greenBright('--start')}\tStarts the app
+    ${chalk.greenBright('--build')}\tBuilds the app`);
 }
