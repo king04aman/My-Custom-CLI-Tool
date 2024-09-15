7f54aae5e25efc55df521eefa79f28feec6d2ca9
Aman Kumar <amanrajkumar4@gmail.com>
2024-09-15 22:28:05 +0530

    Add configuration and update

--- test-projects/node_modules/tools/bin/index.js
+++ test-projects/node_modules/tools/bin/index.js
@@ -5,6 +5,8 @@
 
 const arg = require('arg');
 const chalk = require('chalk');
+// const path = require('path');
+const pkgUp = require('pkg-up');
 
 try {
     const args = arg({
@@ -16,6 +18,22 @@ try {
 
     if (args['--start']) {
         // console.log("starting the application");
+        // const pkg = require('./package.json'); // <-- won't work
+
+        // const pkg = require(path.join(process.cwd(), 'package.json'));
+        // TODO: do something with pkg        
+
+        const pkgPath = pkgUp.sync({cwd: process.cwd()});
+        const pkg = require(pkgPath);
+    
+        if (pkg.tool) {
+          console.log('Found configuration', pkg.tool);
+          // TODO: do something with configuration
+        } else {
+          console.log(chalk.yellow('Could not find configuration, using default'));
+          // TODO: get default configuration
+        }
+        
         console.log(chalk.bgCyanBright('Starting the application'));
     }
 } catch (e) {
