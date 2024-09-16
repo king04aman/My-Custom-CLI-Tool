6134e2c6ddf23169fd53b77a9c2bcbe8a280f347
Aman Kumar <amanrajkumar4@gmail.com>
2024-09-17 01:08:32 +0530

    docs: Refactor the code base

--- test-projects/node_modules/tools/bin/index.js
+++ test-projects/node_modules/tools/bin/index.js
@@ -1,12 +1,9 @@
 #! /usr/bin/env node
 
-// console.log("Hello CLI User!!")
-// console.log(process.argv);
-
 const arg = require('arg');
 const chalk = require('chalk');
-// const path = require('path');
-const pkgUp = require('pkg-up');
+const getConfig = require('../src/config/config-mgr');
+const start = require('../src/commands/start');
 
 try {
     const args = arg({
@@ -14,36 +11,17 @@ try {
         '--build': Boolean,
     });
 
-    // console.log(args);
-
     if (args['--start']) {
-        // console.log("starting the application");
-        // const pkg = require('./package.json'); // <-- won't work
-
-        // const pkg = require(path.join(process.cwd(), 'package.json'));
-        // TODO: do something with pkg        
-
-        const pkgPath = pkgUp.sync({cwd: process.cwd()});
-        const pkg = require(pkgPath);
-    
-        if (pkg.tool) {
-          console.log('Found configuration', pkg.tool);
-          // TODO: do something with configuration
-        } else {
-          console.log(chalk.yellow('Could not find configuration, using default'));
-          // TODO: get default configuration
-        }
-        
-        console.log(chalk.bgCyanBright('Starting the application'));
+        const config = getConfig();
+        start(config);
     }
 } catch (e) {
-    // console.log(e.message);
     console.log(chalk.yellow(e.message));
     console.log();
     usage();
 }
 
-function usage(){
+function usage() {
     // console.log(`tool [cmd]
     //     --start\tStarts the application
     //     --build\tBuilds the application`);
