a11fa1d76f473e1ef9f8eba773b87210de2b5c7f
Aman Kumar <amanrajkumar4@gmail.com>
2024-09-18 01:21:02 +0530

    validation for port number using ajv lib

--- test-projects/node_modules/tools/src/config/config-mgr.js
+++ test-projects/node_modules/tools/src/config/config-mgr.js
@@ -1,13 +1,30 @@
 const chalk = require('chalk');
 const { cosmiconfigSync } = require('cosmiconfig');
+const schema = require('./schema.json');
+const betterAjvErrors = require('better-ajv-errors');
+const Ajv = require('ajv').default;
+const ajv = new Ajv({ jsonPointers: true });
 const configLoader = cosmiconfigSync('tool');
 
 module.exports = function getConfig() {
+
   const result = configLoader.search(process.cwd());
+
   if (!result) {
     console.log(chalk.yellow('Could not find configuration, using default'));
     return { port: 1234 };
+
   } else {
+
+    const isValid = ajv.validate(schema, result.config);
+
+    if (!isValid) {
+      console.log(chalk.yellow('Invalid configuration was supplied'));
+      console.log();
+      console.log(betterAjvErrors(schema, result.config, ajv.errors));
+      process.exit(1);
+    }
+
     console.log('Found configuration', result.config);
     return result.config;
   }
